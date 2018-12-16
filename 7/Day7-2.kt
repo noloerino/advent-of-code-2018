b818ce49ca8303d218dd36558b05a5ed3457dec3
noloerino <jhshi07@gmail.com>
2018-12-15 17:40:30 -0800

    finish 7-2

--- 7/Day7-2.kt
+++ 7/Day7-2.kt
@@ -7,64 +7,66 @@ fun main() {
     val re = "\\b[A-Z]\\b".toRegex()
     // maps label to children
     val dependents = mutableMapOf<Char, MutableList<Char>>()
-    val requirements = mutableMapOf<Char, MutableList<Char>>()
+    val requirementsCt = mutableMapOf<Char, Int>()
     for (ln in lines) {
         val results = re.findAll(ln)
         val from = results.elementAt(0).value[0]
         val to = results.elementAt(1).value[0]
         val dep = dependents.getOrPut(from, { mutableListOf() })
         dep.add(to)
-        val req = requirements.getOrPut(to, { mutableListOf() })
-        req.add(from)
+        val req = requirementsCt.getOrElse(to) { 0 }
+        requirementsCt[to] = req + 1
     }
-    val fringe = PriorityQueue<Char>(object : Comparator<Char> {
-        override fun compare(a: Char, b: Char): Int {
-            if (requirements[a]?.size ?: 0 == 0) {
-                if (requirements[b]?.size ?: 0 != 0) {
-                    return Integer.MIN_VALUE;
-                } else {
-                    return a.toInt() - b.toInt()
-                }
-            } else {
-                return Integer.MAX_VALUE;
-            }
+    requirementsCt.putAll(
+            (dependents.keys - requirementsCt.keys)
+            .associateWith { 0 }
+        )
+    fun getNextJob(): Char? {
+        val candidates = requirementsCt
+            .filter { (_, v) -> v == 0 }
+            .keys
+            .toSortedSet()
+        if (candidates.size == 0) {
+            return null
         }
-    })
-    fringe.addAll(dependents.keys - requirements.keys)
-    val seen = mutableSetOf<Char>()
+        val c = candidates.first()!!
+        requirementsCt.remove(c)
+        return c
+    }
     var time = 0
-    val jobs = mutableListOf(0, 0, 0, 0, 0)
-    val ns = mutableListOf('.', '.', '.', '.', '.') // running labels
+    val workers = 5
+    val jobs = MutableList(workers) { 0 }
+    val ns = MutableList(workers) { '.' } // running label 
     // assign job to first available guy; if not then block
-    while (!fringe.isEmpty() || jobs.any { it > 0 }) {
+    while (requirementsCt.isNotEmpty() || jobs.any { it >= 0 }) {
+    //for (j in 0..20) {
         time++
-        println(ns)
         for (i in jobs.indices) {
             if (jobs[i] <= 0) {
                 // update priorities and prio queue with completed job
                 val c = ns[i]
                 if (c != '.' && dependents[c] != null) {
                     for (d in dependents[c]!!) {
-                        requirements[d]!!.remove(c)
-                        fringe.remove(d)
-                        fringe.add(d)
+                        requirementsCt[d] = requirementsCt[d]!! - 1
                     }
                 }
-                var n = fringe.poll()
-                while (n in seen) {
-                    n = fringe.poll()
-                }
+            }
+        }
+        // assignment loop must be separate from job updating
+        for (i in jobs.indices) {
+            if (jobs[i] <= 0) {
+                var n = getNextJob()
                 if (n != null) {
                     jobs[i] = 61 + n.toInt() - 'A'.toInt()
                     ns[i] = n
-                    seen.add(n)
                 } else {
                     ns[i] = '.'
                 }
             }
             jobs[i]--
         }
+        println("$ns $jobs")
     }
-    println(time)
+    println(time - 1)
 }
 
